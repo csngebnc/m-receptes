92faeb89c1559f0125b74f428643bb957b12f424
Csönge Bence <bluebence@gmail.com>
2023-04-14 11:09:37 +0200

    refactor: rename android project and packages

--- src-mobile/app/src/androidTest/java/hu/bme/aut/android/receptes/ExampleInstrumentedTest.kt
+++ src-mobile/app/src/androidTest/java/hu/bme/aut/android/receptes/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.mreceptes
+package hu.bme.aut.android.receptes
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.mreceptes", appContext.packageName)
+        assertEquals("hu.bme.aut.android.receptes", appContext.packageName)
     }
 }
