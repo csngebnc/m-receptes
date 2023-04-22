edab460a3faff20cc26847dce90bf7fe24e65523
Csönge Bence <bluebence@gmail.com>
2023-04-22 15:11:52 +0200

    feat: add editor composable, viewmodel and repository

---
+++ src-mobile/app/src/main/java/hu/bme/aut/android/receptes/ui/editor/Editor.kt
@@ -0,0 +1,11 @@
+package hu.bme.aut.android.receptes.ui.editor
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun Editor() {
+    Text(text = "Editor")
+}
