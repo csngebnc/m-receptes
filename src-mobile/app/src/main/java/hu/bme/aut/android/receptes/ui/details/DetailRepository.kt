4a6a73e07a3e373c2797f3d3d03798bb696477e4
Csönge Bence <bluebence@gmail.com>
2023-04-22 15:11:39 +0200

    feat: add details composable, viewmodel and repository

---
+++ src-mobile/app/src/main/java/hu/bme/aut/android/receptes/ui/details/DetailRepository.kt
@@ -0,0 +1,5 @@
+package hu.bme.aut.android.receptes.ui.details
+
+import javax.inject.Inject
+
+class DetailRepository @Inject constructor() {}
---
+++ src-mobile/app/src/main/java/hu/bme/aut/android/receptes/ui/details/RecipeDetails.kt
@@ -0,0 +1,11 @@
+package hu.bme.aut.android.receptes.ui.details
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun RecipeDetails() {
+    Text(text = "Details")
+}
