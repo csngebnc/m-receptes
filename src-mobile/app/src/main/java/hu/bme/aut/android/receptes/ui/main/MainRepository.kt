a517870b0bc7ee1e6e9b48d432afa30a89d0e787
Csönge Bence <bluebence@gmail.com>
2023-04-22 15:10:39 +0200

    feat: add main repository and viewmodel

---
+++ src-mobile/app/src/main/java/hu/bme/aut/android/receptes/ui/main/MainRepository.kt
@@ -0,0 +1,8 @@
+package hu.bme.aut.android.receptes.ui.main
+
+import hu.bme.aut.android.receptes.persistence.RecipeDao
+import javax.inject.Inject
+
+class MainRepository @Inject constructor() {
+    init{}
+}
