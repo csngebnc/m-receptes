e4b3b108bf83d2c504e4e6a48c716c2c16999608
Csönge Bence <bluebence@gmail.com>
2023-04-22 15:10:21 +0200

    feat: add root viewmodel

---
+++ src-mobile/app/src/main/java/hu/bme/aut/android/receptes/ui/root/RootViewModel.kt
@@ -0,0 +1,8 @@
+package hu.bme.aut.android.receptes.ui.root
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class RootViewModel @Inject constructor() : ViewModel() {}
