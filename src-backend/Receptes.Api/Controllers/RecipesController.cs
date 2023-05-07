0dcc67886f2be763895f02db684b9f0e5652bb11
Csönge Bence <bluebence@gmail.com>
2023-05-07 10:51:33 +0200

    feat: implement crud on backend

---
+++ src-backend/Receptes.Api/Controllers/RecipesController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Receptes.Api.Dtos;
+using Receptes.Dal;
+using Receptes.Domain.Entities;
+
+namespace Receptes.Api.Controllers
+{
+    [Route("api/recipes")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        private readonly ReceptesDbContext _context;
+
+        public RecipesController(ReceptesDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("all/{userName}")]
+        public async Task<List<Recipe>> GetAllRecipes(string userName)
+        {
+            return await _context.Recipes
+                .Where(x => x.OwnerUsername == userName)
+                .ToListAsync();
+        }
+
+        [HttpGet("single/{recipeId}")]
+        public async Task<Recipe> GetRecipe(Guid recipeId)
+        {
+            return await _context.Recipes.SingleAsync(x => x.Id == recipeId);
+        }
+
+        [HttpPost]
+        public async Task<Recipe> CreateRecipe(CreateRecipeDto recipeDto)
+        {
+            var recipe = new Recipe
+            {
+                Name = recipeDto.Name,
+                Description = recipeDto.Description,
+                OwnerUsername = recipeDto.OwnerUsername
+            };
+
+            _context.Recipes.Add(recipe);
+
+            await _context.SaveChangesAsync();
+
+            return recipe;
+        }
+
+        [HttpPut]
+        public async Task<Recipe> UpdateRecipe(Recipe recipeToUpdate)
+        {
+            var recipe = await _context.Recipes.SingleAsync(x => x.Id == recipeToUpdate.Id && x.OwnerUsername == recipeToUpdate.OwnerUsername);
+
+            recipe.Name = recipeToUpdate.Name;
+            recipe.Description = recipeToUpdate.Description;
+
+            _context.Recipes.Update(recipe);
+
+            await _context.SaveChangesAsync();
+
+            return recipe;
+        }
+
+        [HttpDelete("delete/{recipeId}")]
+        public async Task DeleteRecipe(Guid recipeId)
+        {
+            var recipe = await _context.Recipes.SingleAsync(x => x.Id == recipeId);
+
+            _context.Remove(recipe);
+
+            await _context.SaveChangesAsync();
+        }
+
+        [HttpDelete("/clear-db")]
+        public async Task ClearDb()
+        {
+            var recipes = await _context.Recipes.ToListAsync();
+
+            _context.RemoveRange(recipes);
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ src-backend/Receptes.Domain/Entities/Recipe.cs
@@ -0,0 +1,10 @@
+﻿namespace Receptes.Domain.Entities
+{
+    public class Recipe
+    {
+        public Guid Id { get; set; }
+        public string OwnerUsername { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
